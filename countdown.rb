9b164dcf8e074677396a1bed352d724788b0c7e4
Kennedy Musau <kennedy.musauu@gmail.com>
2023-06-25 12:25:46 +0300

    completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown counter
+
+    while counter > 0
+        puts "#{counter} SECOND(S)!"
+        counter -= 1
+    end
+
+   "HAPPY NEW YEAR!"
+end
+
+puts countdown 10
+
+def countdown_with_sleep counter
+    while counter > 0
+        puts "#{counter} SECOND(S)!"
+        counter -= 1
+        sleep(5)
+    end
+
+   "HAPPY NEW YEAR!"
 end
